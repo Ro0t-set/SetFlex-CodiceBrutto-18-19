3f7f46f18d042b6609aec26d72e77ac0649d81aa
Ro0t-set <ro0trexustom@gmail.com>
2018-11-10 01:01:30 +0100

    anteprima

--- app/models.py
+++ app/models.py
@@ -45,7 +45,7 @@ class Corso(models.Model):
     studente_referente4 = models.ForeignKey('auth.User', related_name='studente_referente4', blank=True, null=True)
     studente_referente5 = models.ForeignKey('auth.User', related_name='studente_referente5', blank=True, null=True)
     descrizione= models.TextField(blank=True)
-    classi_autori = models.CharField(max_length=40, default="")
+    classi_autori = models.CharField(max_length=40, default="", blank=True, null=True)
     esperti_esterni = models.CharField(max_length=100,blank=True, null=True, default="")
     aule = models.ForeignKey('Aula')
 
